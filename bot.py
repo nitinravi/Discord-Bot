9ca9de5797597fb4cf374a68f8e580f55d0f04ff
nitinravi <nitinravi18@gmail.com>
2020-05-29 16:52:17 +0530

    added 8ball event

--- bot.py
+++ bot.py
@@ -12,6 +12,25 @@ client = commands.Bot(command_prefix=".")
 async def on_ready():
     print("Bot is ready")
 
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ["It is certain.", "It is decidedly so.",
+                 "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply hazy, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
+    await ctx.send(f'Question:{question}\n Answer:{random.choice(responses)}')
 
 @client.event
 async def on_member_join(member):
