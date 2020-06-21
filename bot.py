efa0eea2bc9cc93ccc037b1cfc31ba47dc3e1232
nitinravi <nitinravi18@gmail.com>
2020-06-21 15:57:56 +0530

    debugging hosting errors

--- bot.py
+++ bot.py
@@ -1,15 +1,1 @@
-import os
-import discord
-from discord.ext import commands
-
-token = os.getenv('Token')
-
-client = commands.Bot(command_prefix=".")
-
-@client.command()
-async def foo(ctx, params):
-    await ctx.send(f"lorem ipsum")
-
-assert 1 == 2
-
-client.run(token)
+print("Hello world")
